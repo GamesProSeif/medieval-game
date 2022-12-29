5f621a6fa02c928d88cc6f9c7d407bb25454b18d
GamesProSeif <seifmansour2014@outlook.com>
2022-12-29 22:50:34 +0200

    Added fire grenade

--- Assets/Scripts/ProjectileAddon.cs
+++ Assets/Scripts/ProjectileAddon.cs
@@ -6,8 +6,13 @@ using UnityEngine.Timeline;
 public class ProjectileAddon : MonoBehaviour
 {
     public int damage;
+    public int radius;
+    public GameObject fireParticles;
     private Rigidbody rb;
     private bool targetHit;
+
+    public enum WeaponType { Projectile, Grenade };
+    public WeaponType type;
 
     private void Start()
     {
@@ -16,18 +21,36 @@ public class ProjectileAddon : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (targetHit)
-            return;
-        targetHit = true;
 
-        if (collision.gameObject.GetComponent<Enemy>() != null)
+        if (type == WeaponType.Projectile)
         {
-            Enemy enemy = collision.gameObject.GetComponent<Enemy>();
-            Destroy(gameObject);
-            enemy.takeDamage(damage);
+            if (targetHit)
+                return;
+            targetHit = true;
+            if (collision.gameObject.GetComponent<Enemy>() != null)
+            {
+                Enemy enemy = collision.gameObject.GetComponent<Enemy>();
+                enemy.takeDamage(damage);
+                Destroy(gameObject);
+            }
+            rb.isKinematic = true;
+            transform.SetParent(collision.transform);
+        } else if (type == WeaponType.Grenade)
+        {
+            if (collision.gameObject.tag == "Ground")
+            {
+                GameObject particles = Instantiate(fireParticles, transform.position, fireParticles.transform.rotation);
+
+                SphereCollider collider = particles.GetComponent<SphereCollider>();
+
+                // Invoke(nameof(destroy), 5);
+                Destroy(gameObject);
+                rb.isKinematic = true;
+                transform.SetParent(collision.transform);
+            }
         }
 
-        rb.isKinematic = true;
-        transform.SetParent(collision.transform);
+
     }
+
 }
